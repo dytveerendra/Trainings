96dc8e56bbf4113e9fab3f5b9df47d77bc669972
dytveerendra <63532874+dytveerendra@users.noreply.github.com>
2020-05-06 11:26:26 +0530

    First Commit

---
+++ src/com/tm/BaseClass/Config.java
@@ -0,0 +1,14 @@
+package com.tm.BaseClass;
+
+import org.openqa.selenium.WebDriver;
+
+
+public class Config {
+	
+	public static WebDriver driver;
+	public static String CurrentTestCaseName;
+	public static String appURL = "http://testingmasters.com/hrm/";
+	public String Browser = "chrome";
+	public static String TestDataPath = System.getProperty("user.dir")+"\\TestData\\HRM.xlsx";
+		
+}
